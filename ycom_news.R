33db10561d9b9f9e4e99a537c167cfe75c98be08
ragav208 <ragav208@gmail.com>
2019-02-05 17:39:01 +0800

    clean-up and sublinks addition

--- ycom_news.R
+++ ycom_news.R
@@ -1,11 +1,30 @@
 library(rvest)
 content <- read_html('https://news.ycombinator.com/')
-title <- content %>% html_nodes('a.storylink') %>% html_text()
+# title <- content %>% html_nodes('a.storylink') %>% html_text() 
 
-link_domain <- content %>% html_nodes('span.sitestr') %>% html_text()
-score <- content %>% html_nodes('span.score') %>% html_text()
-score = c(1,score)
-age <- content %>% html_nodes('span.age') %>% html_text()
+details <- content %>% html_nodes('a.storylink') %>% html_attr("href") %>%
+  as.data.frame() %>% rename("link"=".") %>%
+  mutate(title = content %>% html_nodes('a.storylink') %>% html_text()) %>%
+  mutate(score = c(content %>% html_nodes('span.score') %>% html_text(),NA)) %>% 
+  mutate(age = content %>% html_nodes('span.age') %>% html_text()) %>% 
+  mutate(link = as.character(link)) 
+str(details)
 
-df <- data.frame(title = title, link_domain = link_domain, score = score, age = age)
+i = details$link[8]
 
+ article = c()
+# 
+ for(i in details$link){
+   print(i)
+  if(url.exists(i)){
+   curr_article <- read_html(as.character(i)) %>% html_nodes('p') %>%html_text()
+  curr_article <- paste(curr_article,collapse  = "   ")
+  article <-  c(article, curr_article)
+  }else{
+    article <- c(article,NA)
+    message("url does not exist ")
+  }
+   }
+
+ details$article <- article
+ # length(article)
